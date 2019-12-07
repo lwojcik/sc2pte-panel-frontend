7df3e098cfff168fc2ce23856536200f4291e390
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2019-12-07 18:08:17 +0100

    remove broken test files (#67)
    * remove broken test files
    
    * downgrade ci to older versions
    
    * fix build command
    
    * fix test command
    
    * fix test command

--- src/containers/Config/Config.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Config from './Config';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Config />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
