1913a491c72028019c57e5c0c5c6fe922d2da4a4
Łukasz Wójcik <lwojcik@lukem.net>
2019-12-18 01:13:55 +0100

    minor formatting fixes

--- src/components/ScrollbarContainer/ScrollbarContainer.tsx
+++ src/components/ScrollbarContainer/ScrollbarContainer.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from 'react';
 import { StrollableContainer } from 'react-stroller';
-
 import Scrollbar from 'src/components/Scrollbar/Scrollbar';
 
 interface ScrollbarContainerProps {
@@ -8,12 +7,14 @@ interface ScrollbarContainerProps {
   children: ReactElement;
 }
 
-const ScrollbarContainer = ({ visible, children }: ScrollbarContainerProps) => {
-  return (
-    <StrollableContainer draggable gap={20} bar={() => <Scrollbar visible={visible ?? true} />}>
-      {children}
-    </StrollableContainer>
-  );
-}
+const ScrollbarContainer = ({ visible, children }: ScrollbarContainerProps) => (
+  <StrollableContainer
+    draggable
+    gap={20}
+    bar={() => <Scrollbar visible={visible ?? true} />}
+  >
+    {children}
+  </StrollableContainer>
+);
 
 export default ScrollbarContainer;
