6279fa00494346bf6e2f68c3a0d80b6106ccfc1e
Łukasz Wójcik <lwojcik@lukem.net>
2019-12-19 21:21:08 +0100

    fix broken test

--- src/components/WinLoseBar/WinLoseBar.test.tsx
+++ src/components/WinLoseBar/WinLoseBar.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Win from './Win';
+import WinLoseBar from './WinLoseBar';
 
 it('renders correctly', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Win count={999} />, div);
+  ReactDOM.render(<WinLoseBar wins={999} losses={999} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
