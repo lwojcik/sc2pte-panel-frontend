aaac1ae0e65ed4538b0f4cfe3f27c0e35ca2b46f
Łukasz Wójcik <lwojcik@lukem.net>
2019-12-22 15:23:27 +0100

    simplify WinLoseBar

--- src/components/WinLoseBar/WinLoseBar.tsx
+++ src/components/WinLoseBar/WinLoseBar.tsx
@@ -13,14 +13,10 @@ const cx = classnames.bind(styles);
 const WinLoseBar = ({ wins, losses }: WinLoseBarProps) => {
   const [winPercentage, lossPercentage] = calculateWinRatio(wins, losses);
 
-  const elemWidth = (number: number) => ({
-    width: `${number}%`,
-  });
-
   return (
     <div className={cx('WinLoseBar')}>
-      <div className={cx('wins')} style={elemWidth(winPercentage)} />
-      <div className={cx('losses')} style={elemWidth(lossPercentage)} />
+      <div className={cx('wins')} style={{ width: `${winPercentage}`}} />
+      <div className={cx('losses')} style={{ width: `${lossPercentage}`}} />
     </div>
   );
 };
