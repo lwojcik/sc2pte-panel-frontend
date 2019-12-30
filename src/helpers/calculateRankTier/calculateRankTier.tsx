2ed3ee9c5446dda865ce1ff53b54281cd140cae3
Łukasz Wójcik <lwojcik@lukem.net>
2019-12-30 19:15:25 +0100

    simplify calculateRankTier

--- src/helpers/calculateRankTier/calculateRankTier.tsx
+++ src/helpers/calculateRankTier/calculateRankTier.tsx
@@ -1,16 +1,11 @@
 import { Rank, Tier } from 'src/components/RankImage/RankImage';
 
 const calculateRankTier = (rank: Rank, divisionRank?: number): Tier => {
+  const multiplier = rank === 'grandmaster' ? 2 : 1;
   if (!divisionRank) return 4;
-  if (rank === 'grandmaster') {
-    if (divisionRank <= 16) return 1;
-    if (divisionRank > 16 && divisionRank <= 50) return 2;
-    if (divisionRank > 50 && divisionRank <= 100) return 3;
-    return 4;
-  }
-  if (divisionRank <= 8) return 1;
-  if (divisionRank > 8 && divisionRank <= 25) return 2;
-  if (divisionRank > 25 && divisionRank <= 50) return 3;
+  if (divisionRank <= 8 * multiplier) return 1;
+  if (divisionRank <= 25 * multiplier) return 2;
+  if (divisionRank <= 50 * multiplier) return 3;
   return 4;
 };
 
