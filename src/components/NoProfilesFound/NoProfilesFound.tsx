6a47219a804486aba4d80d4e3952b3c8056228c1
Łukasz Wójcik <lwojcik@lukem.net>
2020-01-01 16:13:12 +0100

    no profiles found placeholder

--- src/components/NoProfilesFound/NoProfilesFound.tsx
+++ src/components/NoProfilesFound/NoProfilesFound.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import classnames from 'classnames/bind';
 import styles from './NoProfilesFound.module.scss';
+import PlaceholderLogo from 'src/components/PlaceholderLogo/PlaceholderLogo';
+
+const cx = classnames.bind(styles);
 
 const NoProfilesFound = () => (
-  <div className={styles.NoProfilesFound}>
-    No profiles found!!!
+  <div className={cx('NoProfilesFound')}>
+    <PlaceholderLogo />
+    <p>No StarCraft II profiles found!</p>
+    <p>Please make sure the extension is configured correctly in your Twitch Creator Dashboard.</p>
   </div>
 );
 
