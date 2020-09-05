c4dcd4c386ac155f1145671208b46538ddbd9623
Łukasz Wójcik <lwojcik@lukem.net>
2020-09-05 02:18:11 +0200

    change expiry environment variable to indicate it uses miliseconds

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,6 +1,6 @@
 const app = {
   defaultLanguage: process.env.REACT_APP_DEFAULT_LANGUAGE || 'en',
-  refreshInterval: Number(process.env.REACT_APP_UPDATE_INTERVAL) || 480000,
+  refreshInterval: Number(process.env.REACT_APP_UPDATE_INTERVAL_MS) || 600000,
   enableTwitchAuth: (process.env.REACT_APP_ENABLE_TWITCH_AUTH === 'true') || false,
   maxProfiles: Number(process.env.REACT_APP_MAXIMUM_PLAYER_PROFILE_COUNT) || 3,
 };
