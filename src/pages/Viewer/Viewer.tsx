11f5b0e22af02ad6d7c10953b40097b6ef12871f
Łukasz Wójcik <lwojcik@lukem.net>
2020-09-06 01:21:25 +0200

    fix project so that it silenty ignores faulty responses

--- src/pages/Viewer/Viewer.tsx
+++ src/pages/Viewer/Viewer.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useTwitchAuth from 'react-twitch-ext-onauthorized';
 import useViewerData from 'hooks/useViewerData/useViewerData';
 import ViewerPanel from 'components/ViewerPanel/ViewerPanel';
 import Unauthorized from 'components/Unauthorized/Unauthorized';
-import { ViewerData } from 'types';
+import { ViewerData, ProfileObject } from 'types';
 import appConfig from 'config/app';
 
 const { refreshInterval } = appConfig;
 
 const Viewer = () => {
   const { authorized, channelId, token } = useTwitchAuth();
+
   const { error, data } = useViewerData({
     channelId,
     token,
     refreshInterval,
   }) as ViewerData;
 
+  const [userProfiles, setUserProfiles] = useState([] as ProfileObject[]);
+
+  useEffect(() => {
+    data?.profiles?.length > 0 && setUserProfiles(data.profiles);
+  }, [ data ]);
+
   return authorized
     ? (
       <ViewerPanel
         error={error}
-        data={data}
+        profiles={userProfiles}
       />
     )
-    : (
-      <Unauthorized />
-    );
+    : <Unauthorized />;
 };
 
 export default Viewer;
