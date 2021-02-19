276e1df6ca7103c0c9314fa71bda65ab88e766b7
Łukasz Wójcik <lwojcik@lukem.net>
2021-02-19 19:24:16 +0100

    restructuring

--- src/helpers/calculateRankTier/calculateRankTier.test.ts
+++ src/helpers/calculateRankTier/calculateRankTier.test.ts
@@ -1,8 +1,17 @@
 import calculateRankTier from './calculateRankTier';
-import {
-  ranks,
-  divisionRanks,
-} from 'testObjects';
+import { Rank } from 'types';
+
+const ranks = [
+  'bronze',
+  'silver',
+  'gold',
+  'platinum',
+  'diamond',
+  'master',
+  'grandmaster',
+] as Rank[];
+
+const divisionRanks = [1, 3, 6, 10, 20, 26, 40, 51, 72, 99, 101, 125, 174, 200, 202];
 
 it('should return 4 when no division rank is provided', () => {
   ranks.map(rank => expect(calculateRankTier(rank)).toBe(4));
