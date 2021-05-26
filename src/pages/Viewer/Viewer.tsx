8b2690002b9f55ae4a9623b7b2abe696286f305c
Łukasz Wójcik <lwojcik@lukem.net>
2021-05-26 03:07:39 +0200

    fixed missing import

--- src/pages/Viewer/Viewer.tsx
+++ src/pages/Viewer/Viewer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useTwitchAuth from 'react-twitch-ext-onauthorized';
+import { useTwitchAuth } from 'react-twitch-ext-onauthorized';
 import useViewerData from 'hooks/useViewerData/useViewerData';
 import ViewerPanel from 'components/ViewerPanel/ViewerPanel';
 import Unauthorized from 'components/Unauthorized/Unauthorized';
