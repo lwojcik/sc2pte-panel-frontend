5e43c0733f2016ab4767cfe6fa42fb96c6036002
Łukasz Wójcik <lwojcik@lukem.net>
2022-01-01 23:33:36 +0100

    Fix typings

--- src/helpers/constructProfileUrls/constructProfileUrls.tsx
+++ src/helpers/constructProfileUrls/constructProfileUrls.tsx
@@ -1,10 +1,11 @@
-import {
-  StarCraft2API,
-  PlayerObject,
-} from 'starcraft2-api';
+import { StarCraft2API, PlayerObject, ProfileUrlLocale } from "starcraft2-api";
 
 const constructProfileUrls = (profiles: PlayerObject[]) =>
   profiles.map((profile: PlayerObject) =>
-    StarCraft2API.constructProfileUrl({ ...profile }, profile.locale));
+    StarCraft2API.constructProfileUrl(
+      { ...profile },
+      profile.locale as ProfileUrlLocale
+    )
+  );
 
 export default constructProfileUrls;
